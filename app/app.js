8bf3f39688c58bebd63ac23327c126684b105f42
Ted Pennington <tedpennington@Teds-MacBook-Pro.local>
2017-09-05 14:57:20 -0500

    created inital files, angular hooked up and running

---
+++ app/app.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const app = angular.module("ManterestApp", ["ngRoute"]);
+
+// let isAuth = (userFactory) => new Promise ( (resolve, reject) => {
+//   console.log("userFactory is", userFactory);
+//   userFactory.isAuthenticated()
+//   .then( (userExists) => {
+//     if(userExists){
+//       console.log("Authenticated, go ahead");
+//       resolve();
+//     }else {
+//       console.log("Authentication reject, GO AWAY");
+//       reject();
+//     }
+//   });
+// });
+
+
+
+// app.config(($routeProvider) => {
+// 	$routeProvider
+// 	.when('/', {
+// 		templateUrl: 'partials/list.html',
+// 		controller: 'listCtrl',
+// 		resolve: {isAuth}
+// 	})
+// 	.when('/login',{
+// 		templateUrl: 'partials/user.html',
+// 		controller: 'userCtrl'
+// 	})
+// 	.when('/task-list', {
+// 		templateUrl: 'partials/list.html',
+// 		controller: 'listCtrl',
+// 		resolve: {isAuth}
+// 	})
+// 	.when('/item/newItem', {
+// 		templateUrl: 'partials/form.html',
+// 		controller: 'addTaskCtrl',
+// 		resolve: {isAuth}
+// 	})
+// 	.when('/task/:itemId', {
+// 		templateUrl: 'partials/details.html',
+// 		controller: 'detailTaskCtrl',
+// 		resolve: {isAuth}
+// 	})
+// 	.when('/task/:itemId/edit', {
+// 		templateUrl: 'partials/form.html',
+// 		controller: 'editTaskCtrl',
+// 		resolve: {isAuth}
+// 	})
+// 	.otherwise('/');
+// });
+
+
+// app.run(($location, FBCreds) => {
+// 	let creds = FBCreds;
+// 	let authConfig = {
+// 		apiKey: creds.apiKey,
+// 		authDomain: creds.authDomain,
+// 		databaseURL: creds.databaseURL
+// 	};
+
+// 	firebase.initializeApp(authConfig);
+// });
+
+// // example of $rootScope
+// app.run(function($rootScope){
+// 	$rootScope.showSearch = false;
+// });
+
